61780a372c9bd22ea901ae8ab05e9550f7eb64f5
Ianphantom <ian25yola@gmail.com>
2023-10-24 23:18:11 +0700

    refs component add : header component

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 // import component
@@ -6,28 +6,83 @@ import ButtonComponent from "./ButtonComponent";
 
 // import images assets
 import hexavaraLogoHeader from "../assets/images/hexavara_header.png";
+import caretDown from "../assets/svg/caretDown.svg";
+import hamburgerIcon from "../assets/svg/hamburgerIcon.svg";
+import closeIcon from "../assets/svg/closeIcon.svg";
+import { dataProduct } from "../data/data-product";
+
+const ProductMenu = () => {
+  return (
+    <div className='product-sub-list show'>
+      <div className='container'>
+        <div className='row'>
+          {dataProduct.map((item) => (
+            <ProductItemStyled className='col-md-6' key={item.id}>
+              <div className='logo-product'>
+                <img src={item.icon} alt={item.name} />
+              </div>
+              <div className='product-information'>
+                <div className='product-title paragraph-medium'>
+                  {item.name}
+                </div>
+                <div className='small-not-show product-description paragraph-medium regular'>
+                  {item.description}
+                </div>
+              </div>
+            </ProductItemStyled>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const HeaderComponent = () => {
+  const [showProductMenu, setShowProductMenu] = useState(0);
+
+  const productMenuHandler = () => {
+    setShowProductMenu(!showProductMenu);
+  };
+
   return (
     <HeaderComponentStyled>
-      <div className='container'>
+      <div className='container header-container'>
         <div className='logo'>
           <img
             className='pointer'
             src={hexavaraLogoHeader}
             alt='Hexavara Logo'
           />
+          <div className='responsive'>
+            <img src={hamburgerIcon} alt='menu-icon' />
+            <img src={closeIcon} alt='close-icon' />
+          </div>
         </div>
         <div className='navigation'>
           <div className='paragraph-medium regular pointer'>Works</div>
           <div className='paragraph-medium regular pointer'>About Us</div>
-          <div className='paragraph-medium regular pointer'>Products</div>
+          <div
+            className='paragraph-medium regular pointer'
+            onClick={productMenuHandler}
+          >
+            Products{" "}
+            <img
+              src={caretDown}
+              alt='detail-icon'
+              className={`icon ${showProductMenu ? "rotate-180" : ""}`}
+            />
+          </div>
+
+          {showProductMenu ? <ProductMenu /> : ""}
+
           <div className='paragraph-medium regular pointer'>Services</div>
         </div>
-        <ButtonComponent
-          text='Start a Project?'
-          className='button filled-primary border-radius-medium pointer'
-        />
+        <div className='button-container'>
+          <ButtonComponent
+            text='Start a Project?'
+            className='button filled-primary border-radius-medium pointer'
+          />
+        </div>
       </div>
     </HeaderComponentStyled>
   );
@@ -37,11 +92,21 @@ const HeaderComponentStyled = styled.div`
   padding: 24px 0px;
   box-shadow: 0px 12px 16px -4px rgba(0, 0, 0, 0.08),
     0px 4px 6px -2px rgba(16, 24, 40, 0.03);
+  position: relative;
 
-  .container {
+  .header-container {
     display: flex;
     align-items: center;
     justify-content: space-between;
+
+    .logo {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      .responsive {
+        display: none;
+      }
+    }
 
     .navigation {
       display: flex;
@@ -55,6 +120,76 @@ const HeaderComponentStyled = styled.div`
       border-radius: 8px;
       padding: 10px 16px;
       color: white;
+    }
+  }
+
+  .icon {
+    transition: transform 0.5s;
+  }
+
+  .rotate-180 {
+    transform: rotate(180deg);
+  }
+
+  .product-sub-list {
+    padding: 20px 0px;
+    position: absolute;
+    width: 100%;
+    top: 100%;
+    left: 0;
+    background: var(--hxvr-neutral-30);
+    box-shadow: 0px 12px 16px -4px rgba(0, 0, 0, 0.08),
+      0px 4px 6px -2px rgba(16, 24, 40, 0.03);
+  }
+
+  @media (max-width: 768px) {
+    .product-sub-list {
+      position: static;
+    }
+
+    .logo {
+      .responsive {
+        display: block;
+      }
+    }
+
+    .small-not-show {
+      display: none;
+    }
+
+    .header-container {
+      flex-direction: column;
+      align-items: flex-start;
+      .navigation {
+        flex-direction: column;
+      }
+    }
+  }
+
+  @media (min-width: 768px) {
+    .logo {
+      .responsive {
+        display: none;
+      }
+    }
+  }
+`;
+
+const ProductItemStyled = styled.div`
+  background: var(--hxvr-neutral-30);
+  padding: 12px 16px;
+  display: flex;
+  gap: 24px;
+  .product-information {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    .product-title {
+      color: var(--hxvr-neutral-100);
+    }
+
+    .product-description {
+      color: var(--hxvr-neutral-secondary);
     }
   }
 `;
---
+++ src/data/data-product.js
@@ -0,0 +1,59 @@
+import sistemManagementRSIcon from "../assets/images/sistem-manajemen-rs-icon.png";
+import sistemKeuanganDanKoperasi from "../assets/images/sistem-keuangan-dan-koperasi.png";
+import enterpriseResourcePlanning from "../assets/images/enterprise-resource-planning.png";
+import eGovernment from "../assets/images/e-government.png";
+import sistemPengelolaanTokoOnline from "../assets/images/sistem-pengelolaan-toko-online.png";
+import ruangTemu from "../assets/images/ruang-temu.png";
+import inventoryManagementSystem from "../assets/images/inventory-management-system.png";
+import systemPemesananJasa from "../assets/images/sistem-pemesanan-jasa.png";
+
+export const dataProduct = [
+  {
+    id: 1,
+    name: "Sistem Manajemen RS",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    icon: sistemManagementRSIcon,
+  },
+  {
+    id: 2,
+    name: "Sistem Keuangan dan Koperasi",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    icon: sistemKeuanganDanKoperasi,
+  },
+  {
+    id: 3,
+    name: "Enterprise Resource Planning",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    icon: enterpriseResourcePlanning,
+  },
+  {
+    id: 4,
+    name: "E-Government",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    icon: eGovernment,
+  },
+  {
+    id: 5,
+    name: "Sistem Pengelolaan Toko Online",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    icon: sistemPengelolaanTokoOnline,
+  },
+  {
+    id: 6,
+    name: "Ruang Temu",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    icon: ruangTemu,
+  },
+  {
+    id: 7,
+    name: "Inventory Management System",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    icon: inventoryManagementSystem,
+  },
+  {
+    id: 8,
+    name: "Sistem Pemesanan Jasa",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    icon: systemPemesananJasa,
+  },
+];
