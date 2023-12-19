6c48b06940c49ba63aa258730b8c53a9cfde54ae
Ianphantom <ian25yola@gmail.com>
2023-12-19 13:58:36 +0700

    delete unused var

--- src/pages/AboutPages/AboutPagesIndex.js
+++ src/pages/AboutPages/AboutPagesIndex.js
@@ -5,7 +5,6 @@ import AboutPagesSectionOne from "./AboutPagesSectionOne";
 import AboutPagesSectionTwo from "./AboutPagesSectionTwo";
 import CTASectionComponent from "../../components/CTASectionComponent";
 import HappyClientComponent from "../../components/HappyClientComponent";
-import AboutPagesSectionThree from "./AboutPagesSectionThree";
 import AboutPagesSectionFour from "./AboutPagesSectionFour";
 import AboutPagesSectionFive from "./AboutPagesSectionFive";
 import AboutPagesSectionSix from "./AboutPagesSectionSix";
